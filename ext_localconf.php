e79813db4769b3c2d3dc9a341350518d377053f0
Sascha Bruemmer <ping@saschabruemmer.de>
2024-02-28 00:34:17 +0100

    [BUGFIX] Fix TYPO3 constant

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,6 +1,6 @@
 <?php
 
-defined('TYPO3_MODE') or die();
+defined('TYPO3') or die();
 
 // Add processor to replace dynamic database record uid in the site configuration
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['yamlLoader']['placeholderProcessors'][\SomeBdyElse\Coderef\PlaceholderProcessor\Coderef::class] = [];
